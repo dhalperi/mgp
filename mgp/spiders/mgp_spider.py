fc4c2ca3f0b49fb633f419002a577dab3c323600
Daniel Halperin <dhalperi@cs.washington.edu>
2014-04-08 13:52:47 -0700

    mgp_spider: handle missing years

--- mgp/spiders/mgp_spider.py
+++ mgp/spiders/mgp_spider.py
@@ -6,6 +6,12 @@ from urlparse import urlparse, parse_qs, urljoin
 
 from mgp.items import MgpAuthor
 
+def int_or_none(value):
+    try:
+        return int(value)
+    except ValueError:
+        return None
+
 class MgpSpider(Spider):
     name = 'mgp'
     allowed_domains = ['www.genealogy.ams.org']
@@ -17,7 +23,7 @@ class MgpSpider(Spider):
         author['url'] = response.url
         author['mgpid'] = int(parse_qs(urlparse(response.url)[4])['id'][0])
         author['name'] = sel.xpath("//div[@id='paddingWrapper']/h2[1]/text()").extract()[0].strip()
-        author['year'] = int(sel.xpath("//div[@id='paddingWrapper']/div[2]/span/text()[2]").extract()[0])
+        author['year'] = int_or_none(sel.xpath("//div[@id='paddingWrapper']/div[2]/span/text()[2]").extract()[0])
         advisor_a = sel.xpath("//div[@id='paddingWrapper']/p[2]/a")
         author['advisors'] = advisor_a.xpath("./text()").extract()
         advisor_links = advisor_a.xpath('./@href').extract()
